6ed04eb1b237b3b14b537a6fb725bab5c50d558b
Jos Menhart <Jos@estreme.eu>
2019-09-24 20:28:34 +0200

    Add project files.

---
+++ ServiceTestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceTestUtil;
+
+namespace ServiceTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceTestUtil.ServiceTestUtil.StartTimer();        }
+    }
+}
---
+++ ServiceTestLibrary/ServiceTestUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using log4net;
+
+namespace ServiceTestUtil
+{
+    public class ServiceTestUtil
+    {
+        static Timer _timer;
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void StartTimer()
+        {
+            var timer = new Timer(3000);
+            timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+            timer.Enabled = true;
+            _timer = timer;
+            while (true)
+            {
+                System.Threading.Thread.Sleep(2000);
+                Logger.Info("WAITING");
+            }
+
+        }
+
+        static void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // Add DateTime for each timer event.
+            Logger.Info("Hoi");            
+            
+        }
+
+
+    }
+
+}
